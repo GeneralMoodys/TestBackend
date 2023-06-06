e177b7ec9d004027759123c87db7b11e92a2aa72
Dils <adifuadil@gmail.com>
2023-06-06 22:49:27 +0700

    routing api kendaraans, motors, mobils

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerMobil;
+use App\Http\Controllers\ControllerMotor;
+use App\Http\Controllers\ControllerKendaraan;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('Kendaraans', ControllerKendaraan::class);
+Route::apiResource('Motors', ControllerMotor::class);
+Route::apiResource('Mobils', ControllerMobil::class);
